96ab10d7d2c4c8ac3eb49b8c37c49268a2e674e3
Chris Hill <cnh@mit.edu>
1998-06-22 02:11:15 +0000

    Removed debugging return from exch.F ready for release

--- model/inc/SIZE.h
+++ model/inc/SIZE.h
@@ -1,4 +1,4 @@
-C $Header: /u/gcmpack/MITgcm/model/inc/SIZE.h,v 1.10.2.1 1998/06/20 22:20:20 cnh Exp $
+C $Header: /u/gcmpack/MITgcm/model/inc/SIZE.h,v 1.10.2.2 1998/06/22 02:11:15 cnh Exp $
 C
 C     /==========================================================\
 C     | SIZE.h Declare size of underlying computational grid.    |
@@ -36,13 +36,13 @@ C     Nz  - No. points in Z for full process domain.
       INTEGER Nz
       PARAMETER (
      &           sNx =  90,
-     &           sNy =  40,
+     &           sNy =  20,
      &           OLx =   3,
      &           OLy =   3,
      &           nSx =   1,
      &           nSy =   1,
      &           nPx =   1,
-     &           nPy =   1,
+     &           nPy =   2,
      &           Nx  = sNx*nSx*nPx,
      &           Ny  = sNy*nSy*nPy,
      &           Nz  =  20)
