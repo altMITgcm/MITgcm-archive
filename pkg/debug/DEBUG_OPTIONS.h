1c56d2faabd7ccea7eab8a93130878474d311dd3
Alistair Adcroft <aadcroft@princeton.edu>
2003-10-02 18:22:29 +0000

    Missed this file when converting to PACKAGES_CONF.h

---
+++ pkg/debug/DEBUG_OPTIONS.h
@@ -0,0 +1,14 @@
+C $Header: /u/gcmpack/MITgcm/pkg/debug/DEBUG_OPTIONS.h,v 1.1.2.1 2003/10/02 18:22:29 adcroft Exp $
+C $Name:  $
+
+#ifndef DEBUG_OPTIONS_H
+#define DEBUG_OPTIONS_H
+#include "PACKAGES_CONF.h"
+#ifdef ALLOW_DEBUG
+
+#include "CPP_OPTIONS.h"
+
+C CPP Macros go here
+
+#endif /* ALLOW_DEBUG */
+#endif /* DEBUG_OPTIONS_H */
