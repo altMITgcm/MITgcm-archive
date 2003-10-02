6e8dae2bab566c7178b29158d739be19d6e6c67b
Alistair Adcroft <aadcroft@princeton.edu>
2003-10-02 18:22:02 +0000

    Misspelled BULK_FORCE as BULF_FORCE so missed this file.

---
+++ pkg/bulk_force/BULK_FORCE_OPTIONS.h
@@ -0,0 +1,14 @@
+C $Header: /u/gcmpack/MITgcm/pkg/bulk_force/BULK_FORCE_OPTIONS.h,v 1.1.2.1 2003/10/02 18:22:02 adcroft Exp $
+C $Name:  $
+
+#ifndef BULK_FORCE_OPTIONS_H
+#define BULK_FORCE_OPTIONS_H
+#include "PACKAGES_CONF.h"
+#ifdef ALLOW_BULK_FORCE
+
+#include "CPP_OPTIONS.h"
+
+C CPP Macros go here
+
+#endif /* ALLOW_BULK_FORCE */
+#endif /* BULK_FORCE_OPTIONS_H */
