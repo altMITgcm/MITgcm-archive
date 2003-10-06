b9bf3dbc77464f79d7d338bd14411a0de90832b1
Ed Hill <ed@eh3.com>
2003-10-06 04:12:29 +0000

    o small changes for package handling

--- pkg/cal/CAL_OPTIONS.h
+++ pkg/cal/CAL_OPTIONS.h
@@ -1,11 +1,11 @@
-C $Header: /u/gcmpack/MITgcm/pkg/cal/CAL_OPTIONS.h,v 1.1.2.2 2003/10/02 18:30:07 adcroft Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/cal/CAL_OPTIONS.h,v 1.1.2.3 2003/10/06 04:12:29 edhill Exp $
 C $Name:  $
 
 #ifndef CAL_OPTIONS_H
 #define CAL_OPTIONS_H
 #include "PACKAGES_CONFIG.h"
 #ifdef ALLOW_CAL
-
+#define ALLOW_CALENDAR
 #include "CPP_OPTIONS.h"
 
 C CPP macros go here
