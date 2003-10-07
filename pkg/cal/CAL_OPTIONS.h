fd77a6e0dabf928f25c166f98d0fabc856903167
Alistair Adcroft <aadcroft@princeton.edu>
2003-10-07 20:46:27 +0000

    Fix for interdependancy of packages

--- pkg/cal/CAL_OPTIONS.h
+++ pkg/cal/CAL_OPTIONS.h
@@ -1,14 +1,25 @@
-C $Header: /u/gcmpack/MITgcm/pkg/cal/CAL_OPTIONS.h,v 1.1.2.3 2003/10/06 04:12:29 edhill Exp $
+C $Header: /u/gcmpack/MITgcm/pkg/cal/CAL_OPTIONS.h,v 1.1.2.4 2003/10/07 20:46:27 adcroft Exp $
 C $Name:  $
 
 #ifndef CAL_OPTIONS_H
 #define CAL_OPTIONS_H
 #include "PACKAGES_CONFIG.h"
 #ifdef ALLOW_CAL
-#define ALLOW_CALENDAR
+
 #include "CPP_OPTIONS.h"
 
-C CPP macros go here
+C This package is currently controlled by the ECCO_CPPOPTIONS.h and
+C to avoid compatibility issues you should *NOT* customize this file.
+#define ALLOW_CALENDAR
+
+C This CPP flag must be set for no apparent reason other than to stop
+C cal_readparms() for causing to issue a fatal warning that it is
+C undefined!!!
+C TODO: delete this and related code!? AJA
+#define ALLOW_CAL_NENDITER
+
+#define ALLOW_ECCO
+#include "ECCO_CPPOPTIONS.h"
 
 #endif /* ALLOW_CAL */
 #endif /* CAL_OPTIONS_H */
