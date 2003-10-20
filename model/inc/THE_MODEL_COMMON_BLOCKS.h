d08fb56b7a95050528f58d64b0e8a071e4f31c3c
Ed Hill <ed@eh3.com>
2003-10-20 04:01:31 +0000

    o initial steps towards adding "autodiff" targets to genmake2

--- model/inc/THE_MODEL_COMMON_BLOCKS.h
+++ model/inc/THE_MODEL_COMMON_BLOCKS.h
@@ -1,5 +1,13 @@
-C $Id: THE_MODEL_COMMON_BLOCKS.h,v 1.1.1.1 1998/04/22 19:15:31 cnh Exp $
-C
+C $Header: /u/gcmpack/MITgcm/model/inc/THE_MODEL_COMMON_BLOCKS.h,v 1.7 2003/10/09 04:19:18 edhill Exp $
+C $Name:  $
+
+#include "PACKAGES_CONFIG.h"
+
+#include "PARAMS.h"
+#include "GRID.h"
 #include "DYNVARS.h"
 #include "FFIELDS.h"
-#include "CG2D.h"
+#include "SURFACE.h"
+#ifdef ALLOW_TIMEAVE
+#include "TIMEAVE_STATV.h"
+#endif
---
+++ pkg/ptracers/PTRACERS_OPTIONS.h
@@ -0,0 +1,18 @@
+C $Header: /u/gcmpack/MITgcm/pkg/ptracers/PTRACERS_OPTIONS.h,v 1.3 2003/10/09 04:19:20 edhill Exp $
+C $Name:  $
+
+C CPP options file for PTRACERS package
+C
+C Use this file for selecting options within the PTRACERS package
+
+#ifndef PTRACERS_OPTIONS_H
+#define PTRACERS_OPTIONS_H
+#include "PACKAGES_CONFIG.h"
+#ifdef ALLOW_PTRACERS
+
+#include "CPP_OPTIONS.h"
+
+C CPP Macros go here
+
+#endif /* ALLOW_PTRACERS */
+#endif /* PTRACERS_OPTIONS_H */
