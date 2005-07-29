7aa62bb2d57ddbb1317daf0278c9ddecd86d26d0
cvs-fast-export <cvs-fast-export>
2005-07-29 17:57:47 +0000

    Synthetic commit for tag checkpoint57n_post

--- verification/fizhi-cs-aqualev10/code/fizhi_SIZE.h
+++
@@ -1,7 +0,0 @@
-C $Header: /u/gcmpack/MITgcm/verification/fizhi-cs-aqualev10/code/Attic/fizhi_SIZE.h,v 1.1 2005/05/12 18:56:18 molod Exp $
-C $Name:  $
-
-c Physics Grid Vertical Dimension
-c -------------------------------
-      integer Nrphys
-      parameter (Nrphys = 20)
